0ec7803b14415dedbb7547c59750598f3ae50753
RagulskiyDmytriy <93878918+RagulskiyDmytriy@users.noreply.github.com>
2025-05-29 22:26:30 +0300

    First Controller and Route

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\V1\TasksController;
+Route::prefix('v1')->group(function (){
+    Route::apiResource('/tasks', \App\Http\Controllers\Api\V1\TasksController::class);
+});
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
